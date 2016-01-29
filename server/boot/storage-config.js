95c2441f4b90e5ce7c8e1b1e6808a343df35cf40
Trong Thao <trongthaonh@gmail.com>
2016-01-29 17:05:43 +0700

    Add storage config

---
+++ server/boot/storage-config.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+  app.dataSources.systemStorage.connector.getFilename = function(fileInfo, req, res) {
+    var origFilename = fileInfo.name;
+    // optimisticly get the extension
+    var parts = origFilename.split('.'),
+
+    // Using a local timestamp + user id in the filename (you might want to change this)
+    var newFilename = new Array(30).join().replace(/(.|$)/g, function(){
+        return ((Math.random()*36)|0).toString(36);
+      });
+    return "loichuctet.net_" + newFilename + '.'+ "png";
+  };
+};
